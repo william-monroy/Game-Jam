eacfd916022f905c9c55ef1363041de40e61dc51
William Monroy <A00829796@itesm.mx>
2021-04-07 21:26:36 -0500

    i antd

--- src/Components/Horario/index.jsx
+++ src/Components/Horario/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Horario.css'
+import { Timeline } from 'antd'
 
 const Horario = () => {
     return (
-        <div className="container">
-            
+        <div className="">
+            <Timeline mode="left">
+                <Timeline.Item label="2015-09-01">Create a services</Timeline.Item>
+                <Timeline.Item label="2015-09-01 09:12:11">Solve initial network problems</Timeline.Item>
+                <Timeline.Item>Technical testing</Timeline.Item>
+                <Timeline.Item label="2015-09-01 09:12:11">Network problems being solved</Timeline.Item>
+            </Timeline>
         </div>
     )
 }
